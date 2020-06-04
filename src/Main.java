12d94ac62af3c31da98f35a8623738d5a6927cbb
Anton <sogonyaevjk@mail.ru>
2020-06-04 16:02:00 +0300

    next5

--- src/Main.java
+++ src/Main.java
@@ -21,38 +21,9 @@ public class Main {
         Request request = new Request("C:\\Users\\Антон\\IdeaProjects\\TestData.xml", "https://www.dataaccess.com/webservicesserver/NumberConversion.wso");
         request.init();
         request.action();
-        Main.parseXmlFile(Main.formatXML(request.action()));
-
 
     }
 
     // format the XML in pretty String
-    private static String formatXML(String unformattedXml) {
-        try {
-            Document document = parseXmlFile(unformattedXml);
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            transformerFactory.setAttribute("indent-number", 3);
-            Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            DOMSource source = new DOMSource((Node) document);
-            StreamResult xmlOutput = new StreamResult(new StringWriter());
-            transformer.transform(source, xmlOutput);
-            return xmlOutput.getWriter().toString();
-        } catch (TransformerException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    // parse XML
-    private static Document parseXmlFile(String in) {
-        try {
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            InputSource is = new InputSource(new StringReader(in));
-            return (Document) db.parse(String.valueOf(is));
-        } catch (IOException | ParserConfigurationException | org.xml.sax.SAXException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
 }
--- src/Request.java
+++ src/Request.java
@@ -1,3 +1,16 @@
+import jdk.internal.org.xml.sax.InputSource;
+import org.w3c.dom.Node;
+
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
@@ -5,6 +18,11 @@ import java.net.URL;
 import java.nio.charset.Charset;
 
 public class Request {
+
+    public static void main(String[] args) {
+      
+
+    }
 
     private String bodyPath;
     private String endPoint;
@@ -21,7 +39,7 @@ public class Request {
 
     }
 
-    public String action() {
+    public void action() {
         int random_number = 100 + (int) (Math.random() * 999);
         Client cl1 = new Client(random_number);
         int number = cl1.transfer;
@@ -68,8 +86,37 @@ public class Request {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return outputString;
+        System.out.println(Request.parseXmlFile(Request.formatXML(outputString)));
+
     }
     public void end(){
+    }
+    public static String formatXML(String unformattedXml) {
+        try {
+            Document document = parseXmlFile(unformattedXml);
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            transformerFactory.setAttribute("indent-number", 3);
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource((Node) document);
+            StreamResult xmlOutput = new StreamResult(new StringWriter());
+            transformer.transform(source, xmlOutput);
+            return xmlOutput.getWriter().toString();
+        } catch (TransformerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // parse XML
+    public static Document parseXmlFile(String in) {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            InputSource is = new InputSource(new StringReader(in));
+            return (Document) db.parse(String.valueOf(is));
+        } catch (IOException | ParserConfigurationException | org.xml.sax.SAXException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
