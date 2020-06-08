7f0ed121163d9870c818d6c0eb36bab8fe854e58
Anton <sogonyaevjk@mail.ru>
2020-06-08 11:43:08 +0300

    next5

--- src/MyServlet.java
+++ src/MyServlet.java
@@ -1,1 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
 
+import javax.jnlp.FileContents;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class MyServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+
+    public MyServlet() {
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(request.getInputStream(), Charset.forName("UTF-8")));
+        String line = "";
+        StringBuilder result = new StringBuilder();
+        while ((line = in.readLine()) != null) {
+            result.append(line);
+        }
+        in.close();
+        response.setContentType ("text/html; charset=UTF-8");
+        PrintWriter pw = response.getWriter();
+        pw.println("Ответ на POST запрос: ");
+
+        String requiredString = result.substring(result.indexOf("<status>"), result.indexOf("</status>"));
+        pw.println(requiredString);
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType ("text/html; charset=UTF-8");
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1>Hello! ответ на GET запрос. </h1>");
+        pw.println("</html>");
+    }
+
+}
--- src/Request.java
+++ src/Request.java
@@ -20,7 +20,7 @@ import java.nio.charset.Charset;
 public class Request {
 
     public static void main(String[] args) {
-      
+
 
     }
 
