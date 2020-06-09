eb300d8e15e8f93d53f6974903e419a030ea6037
Anton <sogonyaevjk@mail.ru>
2020-06-09 08:33:41 +0300

    next6

--- src/MyServlet.java
+++ src/MyServlet.java
@@ -1,46 +1,43 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.nio.charset.Charset;
-
-import javax.jnlp.FileContents;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
+@WebServlet(value = "/test")
 public class MyServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
-
-    public MyServlet() {
-    }
-
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(request.getInputStream(), Charset.forName("UTF-8")));
-        String line = "";
-        StringBuilder result = new StringBuilder();
-        while ((line = in.readLine()) != null) {
-            result.append(line);
-        }
-        in.close();
-        response.setContentType ("text/html; charset=UTF-8");
-        PrintWriter pw = response.getWriter();
-        pw.println("Ответ на POST запрос: ");
 
-        String requiredString = result.substring(result.indexOf("<status>"), result.indexOf("</status>"));
-        pw.println(requiredString);
-    }
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Client client = new Client(999);
         response.setContentType ("text/html; charset=UTF-8");
         PrintWriter pw = response.getWriter();
-        pw.println("<html>");
-        pw.println("<h1>Hello! ответ на GET запрос. </h1>");
-        pw.println("</html>");
+        String s = "";
+        if (client.transfer > 10){
+            s = "ok";
+        }else s = "fail";
+        String line = String.format("<status> %s </status>", s);
+        pw.write(line);
+
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Client client = new Client(888);
+        response.setContentType ("text/html; charset=UTF-8");
+        PrintWriter pw = response.getWriter();
+        String s = "";
+        if (client.transfer > 10){
+            s = "ok";
+        }else s = "fail";
+        String line = String.format("<status> %s </status>", s);
+        pw.println(line);
     }
 
 }
